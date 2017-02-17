6fff89b414a7731601106463725d857ec0b84d8c
Mr.cat <fantasy.lian@163.com>
2017-02-17 17:38:21 +0800

    修改内容

---
+++ project/demo/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<div id="app">
+		<h1>Hello App!</h1>
+		<p>
+			<router-link to="/user/foo">/user/foo</router-link>
+			<router-link to="/user/bar">/user/bar</router-link>
+		</p>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+	name: 'app',
+	components: {
+		Hello
+	}
+}
+</script>
+
+<style>
+.container {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ project/demo/src/views/bar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>This id bar!</div>
+</template>
+<script>
+	export default {
+		name: 'bar',
+		data() {
+			return {
+				
+			}
+		}
+	}
+</script>
