0ed7ef962cbc7565007f8b05e06b1a5dffed01e0
fantasylian <fantasy.lian@163.com>
2018-10-15 18:37:44 +0800

    数组连接

---
+++ project/node/array/concat.js
@@ -0,0 +1,10 @@
+/**
+ * concat() 方法用于连接两个或多个数组
+ * 该方法不会改变现有的数组，仅会返回被
+ * 连接数组的一个副本
+ */
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5];
+let arr3 = arr1.concat(arr2);
+console.log(arr1);
+console.log(arr3);
