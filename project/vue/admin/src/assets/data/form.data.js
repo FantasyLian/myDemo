0ae182c85602393c1280d360d0edeee9c423ca01
fantasylian <fantasy.lian@163.com>
2018-11-02 16:04:01 +0800

    add new form files

---
+++ project/vue/admin/src/assets/data/form.data.js
@@ -0,0 +1,15 @@
+/**
+ * @desc 建档表单
+ */
+export let homeForm = {
+	creator: "哈利路亚", //建档人
+	username: '', // 姓名
+	healtyCardNo: '', //健康卡号
+	birthday: '', // 出生日期
+	hasGuardianInfo: false, // 监护人信息补充
+	selectNation: '',	// 选择民族
+	bloodType: {
+		type: '1',
+		rh: '1'
+	}, // 血型
+}
