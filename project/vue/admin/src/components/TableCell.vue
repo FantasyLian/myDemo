694bf612ad8f5ffec96aa1ba0454c7bfa5e7d8c2
fantasylian <fantasy.lian@163.com>
2018-11-15 19:24:18 +0800

    add new components

---
+++ project/vue/admin/src/components/TableCell.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <el-table
+            :data="tableData"
+            :span-method="arraySpanMethod"
+            border
+            style="width: 100%">
+            <el-table-column
+                prop="id"
+                label="检查项目"
+                width="180">
+            </el-table-column>
+            <el-table-column
+                prop="name"
+                label="评分">
+            </el-table-column>
+            <el-table-column
+                prop="amount1"
+                label="得分">
+            </el-table-column>
+        </el-table>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'table-cell',
+        data() {
+            return {
+                tableData: [{
+                    id: '12987122',
+                    name: '王小虎',
+                    amount1: '234',
+                }]
+            }
+        },
+        methods: {
+            arraySpanMethod({ row, column, rowIndex, columnIndex }) {
+				if (rowIndex % 2 === 0) {
+					if (columnIndex === 0) {
+						return [1, 2];
+					} else if (columnIndex === 1) {
+						return [0, 0];
+					}
+				}
+			}
+		}
+    }
+</script>
+<style lang="sass" scoped>
+
+</style>
---
+++ project/vue/admin/src/components/area-linkage.vue
@@ -0,0 +1,72 @@
+<template>
+    <section>
+        <div class="inline select-sm">
+            <div class="inline">
+                <select class="form-control w-ss" name="province" v-model="selectedProvince" @change="chooseProvince">
+                    <option value="">省</option>
+                    <option :value="item.code" :key="item.code" v-for="item in preinstall">{{ item.name }}</option>
+                </select>
+            </div>
+            <div class="inline">
+                <select class="form-control w-ss" name="city" v-model="selectedCity" @change="chooseCity">
+                    <option value="">市</option>
+                    <option :value="item.code" :key="item.code" v-for="item in city">{{ item.name }}</option>
+                </select>
+            </div>
+            <div class="inline">
+                <select class="form-control w-ss" name="district" v-model="selectedDistrict" @change="chooseDistrict">
+                    <option value="">区/县</option>
+                    <option :value="item.code" :key="item.code" v-for="item in district">{{ item.name }}</option>
+                </select>
+            </div>
+            <div class="inline">
+                <select class="form-control input-ss w-sm" name="street" v-model="selectedStreet" @change="chooseStreet">
+                    <option value="">街道/乡镇</option>
+                    <option :value="item.code" :key="item.code" v-for="item in street">{{ item.name }}</option>
+                </select>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+    import {country as preinstall} from '../assets/data/from.preinstall';
+    export default {
+        name: 'area-linkage',
+        // props: {
+        //     province: {
+        //         type: Array,
+        //         default: () => []
+        //     },
+        //     city: {
+        //         type: Array,
+        //         default: () => []
+        //     },
+        //     district: {
+        //         type: Array,
+        //         default: () => []
+        //     },
+        //     street: {
+        //         type: Array,
+        //         default: () => []
+        //     }
+        // },
+        data() {
+            return {
+                preinstall,
+                province: '',
+                city: '',
+                district: '',
+                street: ''
+            }
+        },
+        methods: {
+           
+        },
+        created() {
+            
+        }
+    }
+</script>
+<style lang="less">
+    
+</style>
