9c034b523c0bf29846cec53c186af56bf2a54e1a
fantasylian <fantasy.lian@163.com>
2018-11-29 19:53:50 +0800

    modify ckeditor

---
+++ project/vue/admin/src/components/CKEditorExample.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>文本编辑器</div>
+</template>
+<script>
+export default {
+    name: 'ckeditor-example',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
