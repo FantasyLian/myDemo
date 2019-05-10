88530760f06aee86378970b181186372347cf563
Mr.cat <fantasy.lian@gmail.com>
2019-05-11 00:05:00 +0800

    modify practise1.js

---
+++ project/node/exercise/practise1.js
@@ -0,0 +1,10 @@
+// Array.includes()
+
+function test(fruit) {
+    const redFruit = ['apple', 'strawberry', 'cherry', 'cranberries'];
+
+    redFruit.includes(fruit) ? console.log('red') : console.log('not red')
+}
+
+
+test('apple');
