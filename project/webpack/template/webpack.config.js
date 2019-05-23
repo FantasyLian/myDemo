d0ca874b59007bc370bf0c1b37060711b9abfd30
Mr.cat <fantasy.lian@gmail.com>
2019-05-23 17:53:55 +0800

    modify webpack config file

--- project/webpack/template/webpack.config.js
+++ project/webpack/template/webpack.config.js
@@ -1,18 +1,81 @@
 const path = require('path');
 const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
-
-	entry: './src/main.js', // 项目的入口文件，webpack会从main.js开始，把所有依赖的js都加载打包
-
-	output: {
-		path: path.resolve(__dirname, './dist'),
-		publicPath: './dist/',	// 通过devServer访问路径
-		filename: 'build.js'	// 打包后的文件名
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'build.js'
+    },
+    devServer: {
+        historyApiFallback: true,
+        overlay: true
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
 	},
-	devServer: {
-		historyApiFallback: true,
-		overlay: true
-	}
-
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'vue-style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'vue-style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.sass/,
+				use: [
+					'vue-style-loader',
+					'css-loader',
+					'sass-loader?indentedSyntax'
+				]
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.(png|jpg|gif|svg)/,
+				loader: 'file-loader',
+				options: {
+					name: '[name].[ext]?[hash]'
+				}
+			},
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				options: {
+					loaders: {
+						'scss': [
+							'vue-style-loader',
+							'css-loader',
+							'sass-loader'
+						],
+						'sass': [
+							'vue-style-loader',
+							'css-loader',
+							'sass-loader'
+						]
+					}
+				}
+			}
+		]
+	},
+	plugins: [
+		new VueLoaderPlugin()
+	]
 };
