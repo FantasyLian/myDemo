2e605837361a12ddad239ebeed192e16a98d9d9c
Mr.cat <fantasy.lian@163.com>
2020-04-17 23:38:48 +0800

    add components

--- project/vue/zh_daily/components/item.vue
+++ project/vue/zh_daily/components/item.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        
+    <div class="daily-item">
+        <div class="daily-img" v-if="data.images">
+            <img :src="imgPath + data.images[0]">
+        </div>
+        <div class="daily-title" :class="{ noImg: !data.images }">{{ data.title }}</div>
     </div>
 </template>
+<script>
+import $ from '../libs/util';
+export default {
+    props: {
+        data: {
+            type: Object
+        }
+    },
+    data() {
+        return {
+            imgPath: $.imgPath
+        }
+    }
+}
+</script>
