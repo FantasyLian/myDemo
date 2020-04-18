fc31fb2290f39d9369efc970933dd3897d30b71d
Mr.cat <fantasy.lian@163.com>
2020-04-18 23:59:37 +0800

    modify component

--- project/vue/shopping/views/cart.vue
+++ project/vue/shopping/views/cart.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>
-        
+    <div class="cart">
+        <div class="cart-header">
+            <div class="cart-header-title">购物清单</div>
+        </div>
     </div>
 </template>
