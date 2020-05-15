b70bbb197b16eefafef41bd47ae65ee4624252c0
Mr.cat <fantasy.lian@163.com>
2020-05-15 23:13:29 +0800

    使用插件提取css文件

--- project/webpack/demo/webpack.config.js
+++ project/webpack/demo/webpack.config.js
@@ -1,24 +1,36 @@
 'use strict';
 
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
+	// 入口
 	entry: {
 		main: './index.js'
 	},
+	// 出口
 	output: {
 		path: path.join(__dirname, './dist'),
 		publicPath: '/dist/',
 		filename: 'bundle.js'
 	},
+	// 加载器
 	module: {
 		rules: [
 			{
 				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
+				use: ExtractTextPlugin.extract({
+					use: 'css-loader',
+					fallback: 'style-loader'
+				})
 			}
 		]
-	}
+	},
+	// 插件
+	plugins: [
+		// 重命名提取后的css文件
+		new ExtractTextPlugin('main.css')
+	]
 };
 
 module.exports = config;
