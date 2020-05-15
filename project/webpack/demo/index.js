508fca13002b233f5be666c8f14c5272ea0699bf
Mr.cat <fantasy.lian@163.com>
2020-05-16 00:32:48 +0800

    新增vue模块嵌套

--- project/webpack/demo/index.js
+++ project/webpack/demo/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
-import './style.css';
+import Vue from 'vue';
+import App from './app.vue';
 
-document.getElementById('app').innerHTML = 'Hello Webpack.'
+new Vue({
+	el: '#app',
+	render: h => h(App)
+});
--- project/webpack/demo/webpack.config.js
+++ project/webpack/demo/webpack.config.js
@@ -18,6 +18,23 @@ const config = {
 	module: {
 		rules: [
 			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				options: {
+					loaders: {
+						css: ExtractTextPlugin.extract({
+							use: 'css-loader',
+							fallback: 'vue-style-loader'
+						})
+					}
+				}
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
 				test: /\.css$/,
 				use: ExtractTextPlugin.extract({
 					use: 'css-loader',
