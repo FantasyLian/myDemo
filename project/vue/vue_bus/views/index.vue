1d0d75cb89728d03618730201c2816e2898e6c24
Mr.cat <fantasy.lian@163.com>
2020-05-19 02:05:23 +0800

    新增 views

---
+++ project/vue/vue_bus/views/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<h1>随机增加：</h1>
+		<Counter :number="number"></Counter>
+
+		<router-link to="/about">跳转到 about</router-link>
+	</div>
+</template>
+<script>
+	import Counter from '../components/Counter.vue';
+	export default {
+		components: { Counter },
+		methods: {
+			handleAddRandom(num) {
+				this.number += num;
+			}
+		},
+		data() {
+			return {
+				number: 0
+			}
+		},
+		created() {
+			this.$bus.on('add', this.handleAddRandom);
+		},
+		beforeDestory() {
+			this.$bus.off('add', this.handleAddRandom);
+		}
+	}
+</script>
