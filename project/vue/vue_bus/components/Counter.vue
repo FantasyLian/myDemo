7cfcc098440fdb3088e8a69f404c099e42c64440
Mr.cat <fantasy.lian@163.com>
2020-05-19 02:02:05 +0800

    新增组件

---
+++ project/vue/vue_bus/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		{{ number }}
+		<button @click="handleAddRandom">随机新增</button>
+	</div>
+</template>
+<script>
+	export default {
+		props: {
+			number: {
+				type: Number
+			}
+		},
+		methods: {
+			handleAddRandom() {
+				const num = Math.floor(Math.random() * 100 + 1);
+				this.$bus.emit('add', num);
+			}
+		}
+	}
+</script>
