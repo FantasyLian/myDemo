0e823064c188ae68a0251d801f2f581cb719d24d
Mr.cat <fantasy.lian@163.com>
2020-05-19 00:38:54 +0800

    新增 actions

--- project/vue/vuex/views/index.vue
+++ project/vue/vuex/views/index.vue
@@ -3,10 +3,18 @@
 		<h1>首页</h1>
 		{{ count }} 
 		<br>
+		<button @click="handleAsyncIncrement">async +1</button>
+		<br>
 		<br>
 		<button @click="handleIncrement">+2</button>
 		<button @click="handleDecrease">-10</button>
 		<button @click="handleIncrementMore">+5</button>
+		<hr>
+		<br>
+		<br>
+		<div>{{ list }}</div>
+		<br>
+		<div>{{ listCount }}</div>
 		<br>
 		<br>
 		<router-link to="/about">跳转到about</router-link>
@@ -17,6 +25,12 @@
 		computed: {
 			count() {
 				return this.$store.state.count
+			},
+			list() {
+				return this.$store.getters.filteredList
+			},
+			listCount() {
+				return this.$store.getters.listCount
 			}
 		},
 		methods: {
@@ -31,6 +45,11 @@
 			},
 			handleIncrementMore() {
 				this.$store.commit('increment', 5);
+			},
+			handleAsyncIncrement() {
+				this.$store.dispatch('asyncIncrement').then(() => {
+					console.log(this.$store.state.count); // 1
+				});
 			}
 		}
 	}
