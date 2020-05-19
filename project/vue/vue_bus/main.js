db6db815f493ad7d2387237582b62e5bd316366f
Mr.cat <fantasy.lian@163.com>
2020-05-19 22:41:09 +0800

    修改main

--- project/vue/vue_bus/main.js
+++ project/vue/vue_bus/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueBus from './vue-bus.js';
+import App from './app.vue';
+import Routers from './router.js';
+import './style.css';
+
+Vue.use(VueRouter);
+Vue.use(VueBus);
+
+const RouterConfig = {
+	mode: 'history',
+	routes: Routers
+}
+
+const router = new VueRouter(RouterConfig);
+
+router.beforeEach((to, from, next) => {
+	window.document.title = to.meta.title;
+	next();
+});
+
+router.afterEach((to, from, next) => {
+	window.scrollTo(0, 0);
+});
+
+new Vue({
+	el: '#app',
+	router,
+	render: h => h(App)
+})
