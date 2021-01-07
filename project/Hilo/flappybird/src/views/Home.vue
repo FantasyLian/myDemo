bb478dda8634085361cb38c1cabe450402266ae4
Mr.cat <fantasy.lian@163.com>
2021-01-07 19:07:26 +0800

    增加路由器

---
+++ project/Hilo/flappybird/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <flappybird></flappybird>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Flappybird from '@/components/flappybird'
+
+export default {
+  name: 'Home',
+  components: {
+    Flappybird
+  }
+}
+</script>
